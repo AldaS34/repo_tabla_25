d7084ee270369e068fc8fea97a4193a34fff84b8
Carlos Aldair Santana <carlos.santanas@uanl.edu.mx>
2025-04-07 17:07:46 -0600

    metodo mostrarInvertido terminado

--- tabla.py
+++ tabla.py
@@ -6,5 +6,7 @@ class tabla:
         for i in range(1,11):
             print(f"{self.numero} x {i} = {self.numero*i}")
 
-
+    def mostrarInvertido (self):
+        for i in range(10,0,-1):
+            print(f"{self.numero} x {i} = {self.numero*i}")
 
